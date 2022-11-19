31fbcd5eaecba1c1ac91edb0189e6a51732c0453
Paulina Hryciuk <73948761+paulina0127@users.noreply.github.com>
2022-11-19 21:33:16 +0100

    Dodanie views

--- pastry_shop_app/urls.py
+++ pastry_shop_app/urls.py
@@ -1,1 +1,51 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
+    # customers
+    path('customers', views.CustomerList.as_view(),
+         name=views.CustomerList.name),
+    path('customers/<int:pk>', views.CustomerDetail.as_view(),
+         name=views.CustomerDetail.name),
+    # accounts
+    path('accounts', views.AccountList.as_view(),
+         name=views.AccountList.name),
+    path('accounts/<int:pk>', views.AccountDetail.as_view(),
+         name=views.AccountDetail.name),
+    # product types
+    path('product-types', views.ProductTypeList.as_view(),
+         name=views.ProductTypeList.name),
+    path('product-types/<int:pk>', views.ProductTypeDetail.as_view(),
+         name='product-type-detail'),
+    # product flavours
+    path('product-flavours', views.ProductFlavourList.as_view(),
+         name=views.ProductFlavourList.name),
+    path('product-flavours/<int:pk>', views.ProductFlavourDetail.as_view(),
+         name=views.ProductFlavourDetail.name),
+    # product toppers
+    path('product-toppers', views.ProductTopperList.as_view(),
+         name=views.ProductTopperList.name),
+    path('product-toppers/<int:pk>', views.ProductTopperDetail.as_view(),
+         name=views.ProductTopperDetail.name),
+    # products
+    path('products', views.ProductList.as_view(),
+         name=views.ProductList.name),
+    path('products/<int:pk>', views.ProductDetail.as_view(),
+         name=views.ProductDetail.name),
+    # custom products
+    path('custom-products', views.CustomProductList.as_view(),
+         name=views.CustomProductList.name),
+    path('custom-products/<int:pk>', views.CustomProductDetail.as_view(),
+         name=views.CustomProductDetail.name),
+    # order
+    path('orders', views.OrderList.as_view(),
+         name=views.OrderList.name),
+    path('orders/<int:pk>', views.OrderDetail.as_view(),
+         name=views.OrderDetail.name),
+    # order details
+    path('order-details', views.OrderDetailsList.as_view(),
+         name=views.OrderDetailsList.name),
+    path('order-details/<int:pk>', views.OrderDetailsDetail.as_view(),
+         name=views.OrderDetailsDetail.name)
+]
--- pastry_shop_app/views.py
+++ pastry_shop_app/views.py
@@ -1,3 +1,154 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class CustomerList(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    name = 'customer-list'
+    filterset_fields = ['surname', 'name']
+    search_fields = ['id', 'surname', 'name']
+    ordering_fields = ['id', 'surname']
+
+
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    name = 'customer-detail'
+
+
+class AccountList(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    name = 'account-list'
+    search_fields = ['id', 'email']
+    ordering_fields = ['id']
+
+
+class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    name = 'account-detail'
+
+
+class ProductTypeList(generics.ListCreateAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+    name = 'product-type-list'
+    search_fields = ['name']
+    ordering_fields = ['id', 'price']
+
+
+class ProductTypeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+    name = 'product-type-detail'
+
+
+class ProductFlavourList(generics.ListCreateAPIView):
+    queryset = ProductFlavour.objects.all()
+    serializer_class = ProductFlavourSerializer
+    name = 'product-flavour-list'
+    search_fields = ['name']
+    ordering_fields = ['id']
+
+
+class ProductFlavourDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductFlavour.objects.all()
+    serializer_class = ProductFlavourSerializer
+    name = 'product-flavour-detail'
+
+
+class ProductTopperList(generics.ListCreateAPIView):
+    queryset = ProductTopper.objects.all()
+    serializer_class = ProductTopperSerializer
+    name = 'product-topper-list'
+    search_fields = ['name']
+    ordering_fields = ['id', 'price']
+
+
+class ProductTopperDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductTopper.objects.all()
+    serializer_class = ProductTopperSerializer
+    name = 'product-topper-detail'
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    name = 'product-list'
+    filterset_fields = ['type', 'flavour', 'topper']
+    search_fields = ['id', 'name']
+    ordering_fields = ['id', 'price']
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    name = 'product-detail'
+
+
+class CustomProductList(generics.ListCreateAPIView):
+    queryset = CustomProduct.objects.all()
+    serializer_class = CustomProductSerializer
+    name = 'custom-product-list'
+    filterset_fields = ['type', 'flavour', 'topper']
+    search_fields = ['id', 'name']
+    ordering_fields = ['id', 'price']
+
+
+class CustomProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomProduct.objects.all()
+    serializer_class = CustomProductSerializer
+    name = 'custom-product-detail'
+
+
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    name = 'order-list'
+    filterset_fields = ['status', 'order_date', 'pickup_date']
+    search_fields = ['id']
+    ordering_fields = ['id', 'order_date', 'pickup_date', 'total']
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    name = 'order-detail'
+
+
+class OrderDetailsList(generics.ListCreateAPIView):
+    queryset = OrderDetails.objects.all()
+    serializer_class = OrderDetailsSerializer
+    name = 'order-details-list'
+    filterset_fields = ['order']
+    search_fields = ['order']
+    ordering_fields = ['id']
+
+
+class OrderDetailsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderDetails.objects.all()
+    serializer_class = OrderDetailsSerializer
+    name = 'order-details-detail'
+
+
+class ApiRoot(generics.GenericAPIView):
+    name = 'api-root'
+
+    def get(self, request, *args, **kwargs):
+        return Response({'customers': reverse(CustomerList.name, request=request),
+                         'accounts': reverse(AccountList.name, request=request),
+                         'product-types': reverse(ProductTypeList.name, request=request),
+                         'product-flavours': reverse(ProductFlavourList.name, request=request),
+                         'product-toppers': reverse(ProductTopperList.name, request=request),
+                         'products': reverse(ProductList.name, request=request),
+                         'custom-products': reverse(CustomProductList.name, request=request),
+                         'orders': reverse(OrderList.name, request=request),
+                         'order-details': reverse(OrderDetailsList.name, request=request)
+                         })
